f4c6b2c773b6eb3df6b182506c58909181a41585
Rodesar <rockoesp@hotmail.com>
2022-10-21 16:01:34 -0500

    Allow players to add marks to the game board

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
 const gameBoard = (() => {
-    let game = ['X', 'O', 'X', 'O', 'X', 'O', 'X', 'O', 'X'];
+    let game = [];
+    return{game}
+})();
 
-    for (let i = 0; i < 9; i++) {
-        const cell = document.querySelectorAll('.cell');
-        cell[i].textContent = game[i];
+const player = (mark) => {
+    return {mark}
+}
+
+const playerX = player('X');
+
+const playerO = player('O');
+
+const runGame = (() => {
+    const _cells = document.querySelectorAll('.cell');
+    for (let i = 0; i < _cells.length; i++) {
+        const _cell = _cells.item([i])
+        _cell.addEventListener('click', () => {
+            let _player;
+            if (_cell.textContent) {return;}
+            if (!gameBoard.game.length
+                || gameBoard.game.slice(-1) == 'O') {
+                _player = playerX;
+            } else {
+                _player = playerO;
+            }
+            gameBoard.game.push(_player.mark);
+            _cell.textContent = _player.mark;
+        });
     }
-
 })();
