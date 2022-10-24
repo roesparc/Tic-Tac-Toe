7582d4abd7cae4dd5862c1ace2c2b4cf38297706
Rodesar <rockoesp@hotmail.com>
2022-10-24 11:17:25 -0500

    Check when there is 3-in-a-row of the same mark

--- script.js
+++ script.js
@@ -11,9 +11,23 @@ const player = (() => {
 
 const gameBoard = (() => {
     let _board = [];
+    let _playerXGame = [];
+    let _playerOGame = [];
+    const _winningCombinations = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
 
-    function boardAdd(mark) {
+    function boardAdd(mark, cell) {
         _board.push(mark);
+        
+        cell.textContent = mark;
     }
 
     function getMark(){
@@ -28,19 +42,42 @@ const gameBoard = (() => {
         return _selectedPlayer.mark;
     }
 
-    return{boardAdd, getMark}
+    function playerGameAdd(mark, cells, cell) {
+        const boardCell = cells.indexOf(cell);
+
+        (mark == 'X') ?
+        _playerXGame.push(boardCell)
+        :
+        _playerOGame.push(boardCell);
+    }
+
+    function checkWinner() {
+        for (let i = 0; i < _winningCombinations.length; i++) {
+            if (_winningCombinations[i].every(c => _playerXGame.includes(c))) {return true;}
+        }
+
+        for (let i = 0; i < _winningCombinations.length; i++) {
+            if (_winningCombinations[i].every(c => _playerOGame.includes(c))) {return true;}
+        }
+    }
+
+    // console.log()
+
+    return {boardAdd, getMark, playerGameAdd, checkWinner}
 })();
 
 const runGame = (() => {
-    const _cells = document.querySelectorAll('.cell');
+    const _cells = [...document.querySelectorAll('.cell')];
     
     _cells.forEach(cell => cell.addEventListener('click', () => {
-        if (cell.textContent) {return;}
-
         const mark = gameBoard.getMark();
 
-        gameBoard.boardAdd(mark);
-        
-        cell.textContent = mark;
+        if (cell.textContent) {return;}
+
+        if (gameBoard.checkWinner()) {return;}
+
+        gameBoard.boardAdd(mark, cell);
+
+        gameBoard.playerGameAdd(mark, _cells, cell);        
     }));
 })();
