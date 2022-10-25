3bcc654d08d775df4db178068025e38a28d47973
Rodesar <rockoesp@hotmail.com>
2022-10-25 15:23:25 -0500

    Displays player in turn, winner and tie This commit also includes a lot of code organisation, making more functions and variables private.

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 const player = (() => {
-    const playerX = _createPlayer('X');
-    const playerO = _createPlayer('O');
+    let _playerX;
+    let _playerO;
 
-    function _createPlayer(mark) {
-        return {mark}
+    function _createPlayer(mark, name) {
+        return {mark, name}
+    }
+
+    function setName(playerXName, playerOName) {
+        _playerX = _createPlayer('X', playerXName.value);
+
+        _playerO = _createPlayer('O', playerOName.value);
+    }
+
+    function playerXMark() {
+        return _playerX.mark;
+    }
+
+    function playerXName() {
+        return _playerX.name;
+    }
+
+    function playerOMark() {
+        return _playerO.mark;
+    }
+
+    function playerOName() {
+        return _playerO.name;
     }
     
-    return {playerX, playerO}
+    return {
+        setName,
+        playerXMark,
+        playerXName,
+        playerOMark,
+        playerOName
+    }
 })();
 
 const gameBoard = (() => {
@@ -24,60 +52,115 @@ const gameBoard = (() => {
         [2, 4, 6]
     ];
 
-    function boardAdd(mark, cell) {
+    function play(cells, cell, announce) {
+        const mark = _getMark();
+
+        if (_checkWinner(announce)) {return;}
+
+        if (cell.textContent) {return;}
+
+        _boardAdd(mark, cell);
+
+        _playerGameAdd(mark, cells, cell);
+
+        _announcePlayer(mark, announce);
+
+        _checkTie(announce);
+
+        _checkWinner(announce);
+    }
+
+    function _getMark(){
+        let _playerMark;
+
+        if (_board.length % 2 === 0) {
+            _playerMark = player.playerXMark();
+        } else {
+            _playerMark = player.playerOMark();
+        }
+
+        return _playerMark;
+    }
+
+    function _boardAdd(mark, cell) {
         _board.push(mark);
         
         cell.textContent = mark;
     }
 
-    function getMark(){
-        let _selectedPlayer;
+    function _playerGameAdd(mark, cells, cell) {
+        const _boardCell = cells.indexOf(cell);
 
-        if (_board.length % 2 === 0) {
-            _selectedPlayer = player.playerX;
-        } else {
-            _selectedPlayer = player.playerO;
-        }
-
-        return _selectedPlayer.mark;
-    }
-
-    function playerGameAdd(mark, cells, cell) {
-        const boardCell = cells.indexOf(cell);
-
-        (mark == 'X') ?
-        _playerXGame.push(boardCell)
+        (mark === player.playerXMark()) ?
+        _playerXGame.push(_boardCell)
         :
-        _playerOGame.push(boardCell);
+        _playerOGame.push(_boardCell);
     }
 
-    function checkWinner() {
+    function _announcePlayer(mark, announce) {
+        (mark === player.playerOMark()) ?
+        announce.textContent = `${player.playerXName()}'s turn`
+        :
+        announce.textContent = `${player.playerOName()}'s turn`;
+    }
+
+    function _checkTie(announce) {
+        if (_board.length === 9) {
+            announce.textContent = 'It\s a tie!';
+        }
+    }
+
+    function _checkWinner(announce) {
         for (let i = 0; i < _winningCombinations.length; i++) {
-            if (_winningCombinations[i].every(c => _playerXGame.includes(c))) {return true;}
+            if (_winningCombinations[i].every(c => _playerXGame.includes(c))) {
+                announce.textContent = `${player.playerXName()} wins!`;
+                return true;
+            }
         }
 
         for (let i = 0; i < _winningCombinations.length; i++) {
-            if (_winningCombinations[i].every(c => _playerOGame.includes(c))) {return true;}
+            if (_winningCombinations[i].every(c => _playerOGame.includes(c))) {
+                announce.textContent = `${player.playerOName()} wins!`;
+                return true;
+            }
         }
     }
 
     // console.log()
 
-    return {boardAdd, getMark, playerGameAdd, checkWinner}
+    return {play}
 })();
 
 const runGame = (() => {
     const _cells = [...document.querySelectorAll('.cell')];
+    const _board = document.querySelector('.board');
+    const _announce = document.querySelector('.announce');
+    const _startGame = document.querySelector('.start-game');
+    const _playersForm = document.querySelector('form');
+    const _playerXName = document.querySelector('#player-x');
+    const _playerOName = document.querySelector('#player-o');
+
+    _startGame.addEventListener('click', () => {
+        _startGame.style.display = 'none';
+
+        _playersForm.style.display = 'block';
+    });
+
+    _playersForm.addEventListener('submit', (e) => {
+        _playersForm.style.display = 'none';
+
+        _board.style.display = 'grid';
+
+        _announce.style.display = 'block';
+
+        player.setName(_playerXName, _playerOName);
+
+        _announce.textContent = `${player.playerXName()}'s Turn`;
+
+        e.preventDefault();
+    });
     
     _cells.forEach(cell => cell.addEventListener('click', () => {
-        const mark = gameBoard.getMark();
-
-        if (cell.textContent) {return;}
-
-        if (gameBoard.checkWinner()) {return;}
-
-        gameBoard.boardAdd(mark, cell);
-
-        gameBoard.playerGameAdd(mark, _cells, cell);        
+        gameBoard.play(_cells, cell, _announce);
     }));
 })();
