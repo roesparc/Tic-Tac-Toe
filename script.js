aaf2511a9bba1168103776471ff3d8e4569fe4c1
Rodesar <rockoesp@hotmail.com>
2022-10-27 21:08:22 -0500

    Create the logic for the bot mode

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ const player = (() => {
     }
 
     function setName(playerXName, playerOName) {
-        _playerX = _createPlayer('X', playerXName.value);
+        if (playerOName === 'Bot') {
+            _playerX = _createPlayer('X', playerXName.value);
 
-        _playerO = _createPlayer('O', playerOName.value);
+            _playerO = _createPlayer('O', 'Bot');    
+        } else {            
+            _playerX = _createPlayer('X', playerXName.value);
+
+            _playerO = _createPlayer('O', playerOName.value);
+        }    
     }
 
     function playerXMark() {
@@ -54,14 +60,7 @@ const gameBoard = (() => {
         [2, 4, 6]
     ];
 
-    function play(
-        cells,
-        cell,
-        announce,
-        playerXScore,
-        playerOScore,
-        restart
-        ) {
+    function play(cells, cell, announce, playerXScore, playerOScore, restart) {
         const mark = _getMark();
 
         if (announce.textContent.slice(-1) === '!') {return;}
@@ -83,6 +82,38 @@ const gameBoard = (() => {
         _playAgain(announce, restart);
     }
 
+    function playBot(cells, cell, announce, playerXScore, playerOScore, restart) {
+        play(cells, cell, announce, playerXScore, playerOScore, restart);
+
+        _botMove(cells, announce, playerXScore, playerOScore, restart);
+    }
+
+    function _botMove(cells, announce, playerXScore, playerOScore, restart) {
+        const mark = player.playerOMark();
+
+        const cellMove = cells[Math.floor(Math.random() * 9)];
+
+        if (announce.textContent.slice(-1) === '!') {return;}
+
+        if (cellMove.textContent) {
+            _botMove(cells, announce, playerXScore, playerOScore, restart);
+        } else {
+            _boardAdd(mark, cellMove);
+
+            _playerGameAdd(mark, cells, cellMove);
+
+            _announcePlayer(mark, announce);
+
+            _checkTie(announce);
+
+            _checkWinner(announce);
+
+            _updateScore(playerXScore, playerOScore);
+
+            _playAgain(announce, restart);
+        }
+    }
+
     function _getMark(){
         let _playerMark;
 
@@ -97,7 +128,6 @@ const gameBoard = (() => {
 
     function _boardAdd(mark, cell) {
         _board.push(mark);
-        
         cell.textContent = mark;
     }
 
@@ -173,17 +203,22 @@ const gameBoard = (() => {
         _playerOScore = [];
     }
 
-    return {play, restart, backMenu}
+    return {play, playBot, restart, backMenu}
 })();
 
 const runGame = (() => {
+    const _board = document.querySelector('.board');
     const _cells = [...document.querySelectorAll('.cell')];
     const _game = document.querySelector('.game');
     const _announce = document.querySelector('.announce');
     const _startGame = document.querySelector('.start-game');
-    const _playersForm = document.querySelector('form');
+    const _playersForm = document.querySelector('.form-two-players');
+    const _botForm = document.querySelector('.form-bot');
+    const _twoPlayersBtn = document.querySelector('.two-players-button');
+    const _playerBotBtn = document.querySelector('.player-bot-button');
     const _playerXName = document.querySelector('#player-x');
     const _playerOName = document.querySelector('#player-o');
+    const _playerSingleName = document.querySelector('#single-player');
     const _playerXScoreContainer = document.querySelector('.player-x-score');
     const _playerOScoreContainer = document.querySelector('.player-o-score');
     const _restartBtn = document.querySelector('.restart-button');
@@ -199,37 +234,82 @@ const runGame = (() => {
     _playerOScoreContainer.appendChild(_playerOScoreName);
     _playerOScoreContainer.appendChild(_playerOScore);
 
-    _startGame.addEventListener('click', () => {
-        _startGame.style.display = 'none';
-        _playersForm.style.display = 'block';
-    });
+    function formSubmit(e) {
+        if (e.target == _botForm) {
+            _botForm.style.display = 'none';
 
-    _playersForm.addEventListener('submit', (e) => {
-        _playersForm.style.display = 'none';
+            player.setName(_playerSingleName, 'Bot');
+    
+            _playerXScoreName.textContent = _playerSingleName.value;
+            _playerOScoreName.textContent = 'Bot';
+            _board.classList.add('board-bot');    
+        } else {
+            _playersForm.style.display = 'none';
+
+            player.setName(_playerXName, _playerOName);
+    
+            _announce.textContent = `${player.playerXName()}'s turn`;
+            _playerXScoreName.textContent = _playerXName.value;
+            _playerOScoreName.textContent = _playerOName.value;    
+        }
+
+        _twoPlayersBtn.style.display = 'none';
+        _playerBotBtn.style.display = 'none';
         _game.style.display = 'flex';
         _restartBtn.style.display = 'block';
         _menuBtn.style.display = 'block';
 
-        player.setName(_playerXName, _playerOName);
-
-        _announce.textContent = `${player.playerXName()}'s Turn`;
-        _playerXScoreName.textContent = _playerXName.value;
+        _announce.textContent = `${player.playerXName()}'s turn`;
         _playerXScore.textContent = 0;
-        _playerOScoreName.textContent = _playerOName.value;
         _playerOScore.textContent = 0;
 
         e.preventDefault();
+    }
+
+    _startGame.addEventListener('click', () => {
+        _startGame.style.display = 'none';
+        _twoPlayersBtn.style.display = 'block';
+        _playerBotBtn.style.display = 'block';
+    });
+
+    _twoPlayersBtn.addEventListener('click', () => {
+        _playersForm.style.display = 'block';
+        _botForm.style.display = 'none';
+    });
+
+    _playerBotBtn.addEventListener('click', () => {
+        _botForm.style.display = 'block';
+        _playersForm.style.display = 'none';
+    });
+
+    _botForm.addEventListener('submit', (e) => {
+        formSubmit(e);
+    });
+
+    _playersForm.addEventListener('submit', (e) => {
+        formSubmit(e);
     });
 
     _cells.forEach(cell => cell.addEventListener('click', () => {
-        gameBoard.play(
-            _cells,
-            cell,
-            _announce,
-            _playerXScore,
-            _playerOScore,
-            _restartBtn
+        if (_board.classList.value == 'board') {
+            gameBoard.play(
+                _cells,
+                cell,
+                _announce,
+                _playerXScore,
+                _playerOScore,
+                _restartBtn
             );
+        } else {
+            gameBoard.playBot(
+                _cells,
+                cell,
+                _announce,
+                _playerXScore,
+                _playerOScore,
+                _restartBtn
+            );
+        }
     }));
 
     _restartBtn.addEventListener('click', () => {
@@ -249,10 +329,13 @@ const runGame = (() => {
 
         _playerXName.value = '';
         _playerOName.value = '';
+        _playerSingleName.value = '';
         _playerXScore.textContent = 0;
         _playerOScore.textContent = 0;
         _restartBtn.textContent = 'Restart';
         _announce.textContent = `${player.playerXName()}'s turn`;
+
+        _board.classList.remove('board-bot');
 
         _cells.map(c => c.textContent = '');
     });
